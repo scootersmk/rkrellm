3b2ddcfafb8ad1af6d31a2c74e87d46727bda883
Scott Koch <scottkoch@gmail.com>
2016-02-06 13:49:22 -0500

    removed false comment

--- src/rkrellmGeneral.c
+++ src/rkrellmGeneral.c
@@ -17,11 +17,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-/*
- * NOTES
-  -OS_version field currently disabled because it is too long.  
-*/
-
 #include <ncurses.h>
 
 #include <rkrellmStats.h>
